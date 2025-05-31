f620cd25c4d38ca777e2e614caba84aa05562cae
omkark958 <omkarkulkarni958@gmail.com>
2025-05-31 12:55:07 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import "package:provider/provider.dart";
+
+
+
+void main() {
+  // print(Platform.isAndroid?"":"");
+  runApp(App()
+  );
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Text("Welcome")
+      );
+    
+  }
+}
